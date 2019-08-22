aaf3221f3451ee866cd52c1d3c3dba4c9eddc43c
addy81 <adriana.tou@gmail.com>
2019-08-22 12:19:57 +0100

    added comments in extract.py

--- bin/extract.py
+++ bin/extract.py
@@ -35,6 +35,10 @@ class FrequencyOutput:
         pass
 
     def get_output_name(self):
+        """
+        Creates the filename for the list of samples including the frequency calculated.
+        :return: Result filename timestamped and including the query information
+        """
 
         now = datetime.now()
 
@@ -44,6 +48,11 @@ class FrequencyOutput:
         return results_file
 
     def calculate_frequency(self):
+        """
+        Calculates frequency of requested variant, by divided the sum of allele counts of the vatiants identified,
+        divided by the total number of samples in GeminiDB multiplied by two.
+        :return: variant frequency
+        """
 
         allele_count = 0
         for sample in self.results:
@@ -51,6 +60,7 @@ class FrequencyOutput:
 
         frequency = allele_count /( self.total_count*2)
         return frequency
+
 
 def parse_arguments():
     parser = argparse.ArgumentParser(description = 'Extract information from GeminiDB')
@@ -76,28 +86,42 @@ def parse_arguments():
     return arguments
 
 
-def get_variant_id(args):
-    query_chrom = args.Chrom
-    query_position = args.Pos
-    query_ref = args.Ref
-    query_alt = args.Alt
+def get_variant_id(chrom,pos,ref,alt):
+    """
+    Retrieves variant id from GeminiDB.
 
-    # Get variant if=d for variant in question.
-    qVariant = DBSession.query(Variant).filter_by(chrom=query_chrom, pos=query_position, ref=query_ref,
-                                                  alt=query_alt)
+    :param chrom: int -- variant chromosome number
+    :param pos: int -- variant position
+    :param ref: str -- reference base
+    :param alt: str -- reference alternative base
+    :return: Returns variant id from the Variant table.
+    """
 
+    # Query Variant table in database
+    qVariant = DBSession.query(Variant).filter_by(chrom=chrom, pos=pos, ref=ref,
+                                                  alt=alt)
+    # Returns an object list so need to loop through to access the entry.
     for result in qVariant:
         variant_result = result.id
 
     return variant_result
 
+
 def find_samples_with_variant(variant_result):
-    # Query database using variant_id in question
+    """
+    Extract the sample names and variant sequencing information.
+
+    :param variant_result: variant id
+    :return: List of QuerySample objects containing sample name and metrics
+    """
+    # Query database using variant_id in question.
+    # Sample table joins to analysis table, and analysis to analysis variant.
     analyses = DBSession.query(Analysis, AnalysisVariant).join(AnalysisVariant,
                                                                Analysis.id == AnalysisVariant.analysis_id).filter_by(
         variant_id=variant_result).join(Sample).order_by(Sample.name)
 
     query_results = []
+    # a for analysis and av for analysis variant.
     for a, av in analyses:
         result = QuerySample(a.sample.name, av.depth, av.quality, av.AAF, av.allele_count)
         query_results.append(result)
@@ -105,16 +129,35 @@ def find_samples_with_variant(variant_result):
     return query_results
 
 
-def get_frequency(args,query_results):
+def get_frequency(chrom,pos,ref,alt,query_results):
+    """
+    Calculates the frequency of a variant in the Gemini Database
+
+    :param chrom: int -- variant chromosome number
+    :param pos: int -- variant position
+    :param ref: str -- reference base
+    :param alt: str -- reference alternative base
+    :param query_results: List of objects containing sample name and variant metrics.
+    :return: Output filename(str) and frequency result(float)
+    """
+
+    # Get the total number of G(1) and X(2) samples.
     total_count = DBSession.query(Sample).filter(or_(Sample.project_id == 1, Sample.project_id == 2)).count()
-    print (total_count)
-    frequency = FrequencyOutput(args.Chrom, args.Pos, args.Ref, args.Alt, query_results, total_count)
+    frequency = FrequencyOutput(chrom,pos,ref,alt, query_results, total_count)
     output = frequency.get_output_name()
-    gemini_AAF = frequency.calculate_frequency()
+    gemini_aaf = frequency.calculate_frequency()
 
-    return output,gemini_AAF
+    return output,gemini_aaf
 
-def write_to_file(output,gemini_AAF,query_results):
+
+def write_to_file(output,gemini_aaf,query_results):
+    """
+     Writes output file containing sample list with variant metrics and the calculated frequency,
+
+    :param output:str -- filename for output
+    :param gemini_AAF: float -- frequency result
+    :param query_results: List of objects containing sample name and variant metrics.
+    """
     with open(output, "+w") as out:
         output_writer = csv.writer(out, delimiter='\t', quotechar='"', quoting=csv.QUOTE_MINIMAL)
         output_writer.writerow(['Sample', 'Depth', 'Qual', 'AAF', 'Allele_Count'])
@@ -124,17 +167,22 @@ def write_to_file(output,gemini_AAF,query_results):
                 [variant.name, variant.depth, variant.quality, variant.AAF, variant.allele_count])
 
         output_writer.writerow([])
-        output_writer.writerow(['Gemini', 'Frequency', gemini_AAF])
+        output_writer.writerow(['Gemini', 'Frequency', gemini_aaf])
 
-    print("The frequency of the requested variant is :", round(gemini_AAF, 5))
+    print("The frequency of the requested variant is :", round(gemini_aaf, 5))
     print('\n *** A .txt file containing the list of samples has been successfully created. ***')
     print("Filename: ", output)
 
 
-def get_panel_id(args):
+def get_panel_id(panel):
+    """
+    Interrogates database to get panel id from Panel table.
 
-    panel_name = args.panel
-    qPanel = DBSession.query(Panel).filter_by(name=panel_name)
+    :param panel: str - name of panel (has to be exactly the same as the one in the database)
+    :return: int -- panel table id for requested panel
+    """
+
+    qPanel = DBSession.query(Panel).filter_by(name=panel)
 
     for result in qPanel:
         panel_id = result.id
@@ -142,10 +190,15 @@ def get_panel_id(args):
     return panel_id
 
 
-def get_gene_id(args):
+def get_gene_id(gene):
+    """
+    Interrogates database to get gene id in question
 
-    gene_name = args.gene
-    qGene = DBSession.query(Gene).filter_by(name=gene_name)
+    :param gene: str -- gene name
+    :return: Gene if from gene table
+    """
+
+    qGene = DBSession.query(Gene).filter_by(name=gene)
 
     for result in qGene:
         gene_id = result.id
@@ -153,9 +206,15 @@ def get_gene_id(args):
     return gene_id
 
 def get_genes(panel_id):
+    """
+    Finds the genes included in the requested panel
 
-    #qGenes = DBSession.query(GenePanel).join(Panel,Panel.id==GenePanel.panel_id)\
-    #   .join(Gene, Gene.id == GenePanel.gene_id).filter(Panel.id==panel_id)
+    :param panel_id: int -- id of panel
+    :return: Dict of genes with gene ids as keys and gene names as values
+    """
+
+    # Queries the database.
+    # Gene to Panels have a many to many relationship which requires the GenePanel table as an association table
 
     qGenes = DBSession.query(Gene).join(GenePanel,GenePanel.gene_id == Gene.id)\
         .join(Panel,Panel.id == GenePanel.panel_id).filter(Panel.id == panel_id)
@@ -166,8 +225,14 @@ def get_genes(panel_id):
 
     return genes
 
-def get_transcripts(genes):
 
+def get_transcripts(genes):
+    """
+    Returns transcripts for genes present in the panel
+
+    :param genes: dict -- {gene_id:gene_name}
+    :return: dict -- transcript dictionary with gene names as keys and refseq transcripts as values
+    """
     transcripts = {}
 
     for key in genes.keys():
@@ -179,6 +244,12 @@ def get_transcripts(genes):
     return transcripts
 
 def get_panels(gene_id):
+    """
+    Returns list of panels a specific gene is in
+
+    :param gene_id: int -- id of gene
+    :return: list -- panel names
+    """
 
     panels = []
 
@@ -190,10 +261,16 @@ def get_panels(gene_id):
 
     return panels
 
-def get_samples(panel_name):
+def get_samples(panel):
+    """
+    Creates a list of samples that have been tested for a specific panel
+
+    :param panel: str -- panel name
+    :return: list of samples
+    """
 
     qSamples = DBSession.query(Sample).join(SamplePanel,SamplePanel.sample_id==Sample.id)\
-        .filter(SamplePanel.panel_name == panel_name).order_by(Sample.name)
+        .filter(SamplePanel.panel_name == panel).order_by(Sample.name)
 
     samples = []
     for entry in qSamples:
@@ -202,6 +279,16 @@ def get_samples(panel_name):
     return samples
 
 def panel_output(args,genes,transcripts,samples):
+
+    """
+    Writes appropriate file depending on the argument used
+
+    :param args: arguments parsed
+    :param genes: dict of genes
+    :param transcripts: dict of transcripts
+    :param samples: list of samples
+
+    """
 
     if args.t:
         filename = args.panel + "_transcripts.txt"
@@ -223,40 +310,50 @@ def panel_output(args,genes,transcripts,samples):
                 f.write("{}\n".format(genes[key]))
 
 
-def gene_output(args,panels):
-    filename = args.gene + ".txt"
+def gene_output(gene,panels):
+    """
+    Writes file when list of panels tested is requested.
+
+    :param gene: str - gene name
+    :param panels: list of panels
+
+    """
+    filename = gene + ".txt"
 
     with open (filename,'w+') as f:
-        f.write("{}\n".format(args.gene))
+        f.write("{}\n".format(gene))
         for panel in panels:
             f.write("{}\n".format(panel))
 
-def main(args):
 
+def main(args):
 
     if args.subparser_command == 'calculate_geminiAF':
 
-        variant_result = get_variant_id(args)
+        chrom = args.Chrom
+        pos = args.Pos
+        ref = args.Ref
+        alt = args.Alt
+
+        variant_result = get_variant_id(chrom,pos,ref,alt)
         query_results = find_samples_with_variant(variant_result)
-        output, gemini_AAF = get_frequency(args,query_results)
+        output, gemini_AAF = get_frequency(chrom,pos,ref,alt,query_results)
         write_to_file(output,gemini_AAF,query_results)
 
     elif args.subparser_command == 'get_panel_genes':
 
-
         if args.gene:
-            gene_id = get_gene_id(args)
+            gene = args.gene
+            gene_id = get_gene_id(gene)
             panels = get_panels(gene_id)
-            gene_output(args,panels)
+            gene_output(gene,panels)
         elif args.panel:
-            panel_name = args.panel
-            panel_samples = get_samples(panel_name)
-            panel_id = get_panel_id(args)
+            panel = args.panel
+            panel_samples = get_samples(panel)
+            panel_id = get_panel_id(panel)
             panel_genes = get_genes(panel_id)
             panel_transcripts = get_transcripts(panel_genes)
             panel_output(args,panel_genes,panel_transcripts,panel_samples)
-
-
 
 
 if __name__ == '__main__':
